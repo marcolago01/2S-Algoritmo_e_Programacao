80fb951ce72bdbfb5a005e952493fa230b2c3e15
Marco Dionatan Lago <marcolago01@hotmail.com>
2017-08-08 21:35:10 -0300

    Resolução de Exercicios Aula 1 - Programação segundo semestre

---
+++ Exercicios1-1/Exercicios1-1/Exercicios1-1.cpp
@@ -0,0 +1,12 @@
+// Exercicios1-1.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Ola Mundo" << endl;
+}
