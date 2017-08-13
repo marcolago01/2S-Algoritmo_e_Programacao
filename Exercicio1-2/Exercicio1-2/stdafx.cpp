2ce03b089acfe4dcf7a516ae3c0803a882ffa38c
Marco Dionatan Lago <marcolago01@hotmail.com>
2017-08-13 12:56:44 -0300

    Organização

--- Exercicio1-2/Exercicio1-2/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : arquivo de origem que inclui apenas as inclusões padrões
-// Exercicio1-2.pch será o cabeçalho pré-compilado
-// stdafx.obj conterá as informações de tipo pré-compiladas
-
-#include "stdafx.h"
-
-// TODO: referencie qualquer cabeçalho adicional necessário em STDAFX.H
-// e não neste arquivo
